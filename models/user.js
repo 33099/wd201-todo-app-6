5cf05e0894ca3f6c3f22dd9074db07ff53d00e01
33099 <divya030401@gmail.com>
2023-01-13 21:38:45 +0530

    Create the 'users' table with a sequelize migration

--- models/user.js
+++ models/user.js
@@ -8,9 +8,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Todo, {
-        foreignKey: "userId",
-      });
       // define association here
     }
   }
