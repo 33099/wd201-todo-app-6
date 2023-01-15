171780140436709a26c9506d92aeb1f011a8499c
33099 <divya030401@gmail.com>
2023-01-15 15:50:16 +0530

    modifying till csrf tokens again

--- models/todo.js
+++ models/todo.js
@@ -8,6 +8,106 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static associate(models) {
+      // define association here
+    }
+    static async overdue() {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueToday() {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueLater() {
+      return await Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async completed() {
+      return await Todo.findAll({
+        where: {
+          completed: true,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async addTodo({ title, dueDate }) {
+      return await this.create({
+        title: title,
+        dueDate: dueDate,
+        completed: false,
+      });
+    }
+    static async remove(id) {
+      return await this.destroy({
+        where: {
+          id: id,
+        },
+      });
+    }
+
+    setCompletionStatus(status) {
+      return this.update({ completed: status });
+    }
+    static async getTodos() {
+      return await this.findAll();
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
+};
+
+/* wd201 -10
+
+"use strict";
+const { Op } = require("sequelize");
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+
+   remove this for wd201-10 */
+
+/**
+ * Helper method for defining associations.
+ * This method is not a part of Sequelize lifecycle.
+ * The `models/index` file will call this method automatically.
+ */
+
+/* again start from here wd201-10
+
+
+
 
     static associate(models) {
       Todo.belongsTo(models.User, {
@@ -96,3 +196,6 @@ module.exports = (sequelize, DataTypes) => {
   );
   return Todo;
 };
+  
+
+till here wd201-10*/
